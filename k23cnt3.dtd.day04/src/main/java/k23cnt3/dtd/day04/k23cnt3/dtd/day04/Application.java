50e6265652ca1cd81923c44fdb9bc693963e8dfc
DoDuongw <atgdotungduong@gmail.com>
2025-11-18 22:55:25 +0700

    k23cnt3.dtd.Day05

---
+++ k23cnt3.dtd.day04/src/main/java/k23cnt3/dtd/day04/k23cnt3/dtd/day04/Application.java
@@ -0,0 +1,13 @@
+package k23cnt3.dtd.day04.k23cnt3.dtd.day04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
